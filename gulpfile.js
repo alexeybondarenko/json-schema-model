f21cb4338c048b7df72194155538dd308c7a34a3
Alexey Bondarenko <alexeybondarenko@me.com>
2016-06-02 22:28:57 +0300

    add release tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,4 @@
-
+// add to gulpfile.js
 var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
-
-
-gulp.task('release', function () {
-
-  return gulp.src(['./**/*', '!node_modules/**/*','!node_modules'])
-    .pipe(ghPages({
-      branch: 'releases',
-      force: true
-    }))
-});
+// pass along gulp reference to have tasks imported
+require('gulp-release-tasks')(gulp);
